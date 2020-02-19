527a3ebb7fde685f62fbf450ed1b0dd3c72580d4
Giovani Garfias <46204471+survani@users.noreply.github.com>
2020-02-18 20:10:57 -0800

    Initial Update

---
+++ client/src/Movies/UpdateMovie.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const AddMovie = () => {
+  return (
+    <div>
+      <form>
+        <input value="title" type="text" placeholder="Title" />
+      </form>
+    </div>
+  );
+};
+
+export default AddMovie;
